af77808b6c1e1f553c8333f138ab5de4c39c1a45
Luke Hewitt <sensator@gmail.com>
2020-05-30 15:21:00 +1000

    fixing stuff

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function navBarFunction() {
-    var x = document.getElementById("TopNavBar");
-    if (x.className === "navbar") {
-      x.className += "responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
